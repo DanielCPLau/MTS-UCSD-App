b917dd03d827bcad0c3eceaaae9b92fa896ed278
Sheldon <shl322@ucsd.edu>
2016-10-22 19:07:29 -0700

    Added code to grab JSON file of MTS lines and format into String ArrayList

--- app/src/main/java/com/example/daniel/mts/RemoteFetch.java
+++ app/src/main/java/com/example/daniel/mts/RemoteFetch.java
@@ -5,15 +5,33 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
 import android.content.Context;
+import android.content.res.Configuration;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Isaac on 10/22/16.
@@ -153,3 +171,90 @@ public class RemoteFetch {
         }
     }
 }
+
+        //Testing data retrieval from mts api
+//        Button btnHit = (Button)findViewById(R.id.btnHit);
+//        mtsInfo = (TextView) findViewById(R.id.mts);
+//        if(mtsInfo == null)
+//        {
+//            Log.d("nullTest","MTSINFO IS NULL");
+//        }
+//        btnHit.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                new JSONTask().execute("http://realtime.sdmts.com/api/api/where/route-ids-for-agency/MTS.json?key=393bdfb5-b145-4149-a45e-067d8acb6246");
+//            }
+//        });
+
+
+
+//public class JSONTask extends AsyncTask<String, String, String> {
+//
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//        }
+//
+//        @Override
+//        protected String doInBackground(String... params) {
+//            HttpURLConnection connection = null;
+//            BufferedReader reader = null;
+//
+//            try {
+//                URL url = new URL(params[0]);
+//                connection = (HttpURLConnection) url.openConnection();
+//                connection.connect();
+//                InputStream stream = connection.getInputStream();
+//                reader = new BufferedReader(new InputStreamReader(stream));
+//                StringBuffer buffer = new StringBuffer();
+//                String line ="";
+//                while ((line = reader.readLine()) != null){
+//                    buffer.append(line);
+//                }
+//                String finalJson= buffer.toString();
+//                JSONObject parentObject = new JSONObject(finalJson);
+//                JSONObject childObject = (JSONObject)parentObject.get("data");
+//                JSONArray parentArray = childObject.getJSONArray("list");
+//                ArrayList<String> busList = new ArrayList<String>();
+//                if (parentArray != null) {
+//                    int len = parentArray.length();
+//                    for (int i=0;i<len;i++){
+//                        busList.add(parentArray.get(i).toString());
+//                    }
+//                }
+//                String busNum = busList.get(0);
+////                return busNum;
+//                return busList.toString();
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            } finally {
+//                if(connection != null) {
+//                    connection.disconnect();
+//                }
+//                try {
+//                    if(reader != null) {
+//                        reader.close();
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            return null;
+//        }
+//
+//    @Override
+//    protected void onPostExecute(String result) {
+//        super.onPostExecute(result);
+//        if(mtsInfo != null) {
+//            mtsInfo.setText(result);
+//        }
+//    }
+//}
+
+
