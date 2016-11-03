ec8ad1cc6d4d3bb5c851069e2c92b110dd7525f1
wjddn1994 <jwc048@ucsd.edu>
2016-11-02 20:19:30 -0700

    Update MapFragment.java

--- app/src/main/java/com/example/daniel/mts/MapFragment.java
+++ app/src/main/java/com/example/daniel/mts/MapFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,13 +18,18 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -42,6 +49,13 @@ public class MapFragment extends android.support.v4.app.Fragment implements OnFr
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private ListView lv;
+
+    EditText inputSearch;
+
+    CustomListAdapter adapter;
+    //ArrayList<HashMap<String, String>> productList;
 
     private OnFragmentInteractionListener mListener;
 
@@ -70,7 +84,6 @@ public class MapFragment extends android.support.v4.app.Fragment implements OnFr
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
 
@@ -80,22 +93,96 @@ public class MapFragment extends android.support.v4.app.Fragment implements OnFr
         super.onCreate(savedInstanceState);
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.map_activity_list, null);
 
-        String[] buttonName = createButtonList();
-        Integer[] mapId = createMapList();
-        Integer[] iconId = createIconList();
-        ListView lv = (ListView) rootView.findViewById(R.id.android_list);
+        /*
+        final String[] buttonName = createButtonList();
+        final Integer[] mapId = createMapList();
+        final Integer[] iconId = createIconList();
+        */
+        final ListView lv = (ListView) rootView.findViewById(R.id.android_list);
 
+        inputSearch = (EditText) rootView.findViewById(R.id.inputSearch);
+        final ArrayList<String> filterNameArray = createButtonList();
+        final ArrayList<Integer> filterMapArray = createMapList();
+        final ArrayList<Integer> filterIconArray = createIconList();
 
-        CustomListAdapter adapter = new CustomListAdapter(getActivity(), buttonName, mapId, iconId);
+        adapter = new CustomListAdapter(getActivity(), filterNameArray, filterMapArray, filterIconArray);
         lv.setAdapter(adapter);
 
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //MapFragment.this.adapter.getFilter().filter(charSequence);
+                String input = (charSequence.toString()).toLowerCase();
+                int length = input.length();
+                String[] buttonNameLower = new String[filterNameArray.size()];
+
+                for(int k = 0; k < filterNameArray.size(); k++) {
+                    buttonNameLower[k] = filterNameArray.get(k).toLowerCase();
+
+                }
+
+                filterNameArray.clear();
+
+                ArrayList<Integer> copyMapArray = new ArrayList<Integer>(filterMapArray.size());
+                for (int index = 0; index < filterMapArray.size(); index++){
+
+                        copyMapArray.add(filterMapArray.get(index));
+
+                }
+                filterMapArray.clear();
+
+                ArrayList<Integer> copyIconArray = new ArrayList<Integer>(filterIconArray.size());
+                for (int index2 = 0; index2 < filterIconArray.size(); index2++){
+
+                    copyIconArray.add(filterIconArray.get(index2));
+
+                }
+                filterIconArray.clear();
+
+
+                for (int j = 0; j < buttonNameLower.length; j++) {
+                    if (length <= buttonNameLower[j].length()) {
+
+                        if (buttonNameLower[j].toString().contains(input)) {
+                            filterNameArray.add(buttonNameLower[j]);
+                            filterMapArray.add(copyMapArray.get(j));
+                            filterIconArray.add(copyIconArray.get(j));
+
+                        }
+                    }
+                }
+
+
+                adapter = new CustomListAdapter(getActivity(), filterNameArray, filterMapArray, filterIconArray);
+                lv.setAdapter(adapter);
+                /*
+
+                if (Arrays.asList(buttonNameLower).contains(input)){
+
+                    int indexOfInput = Arrays.asList(buttonNameLower).indexOf(input);
+                    adapter = new CustomListAdapter(getActivity(), new String[]{input}, new Integer[]{mapId[indexOfInput]}, new Integer[]{iconId[indexOfInput]});
+                    lv.setAdapter(adapter);
+                }
+                */
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
         // Inflate the layout for this fragment
         return rootView;
     }
 
-    private String[] createButtonList(){
+    private ArrayList<String> createButtonList(){
         String[] buttonName;
+        ArrayList<String> toReturn = new ArrayList<String>();
         buttonName = new String[]{
                 "201", "202", "150","30", "41", "237", "921", "City Shuttle(Arriba/Nobel)",
                 "Coaster East", "Coaster East/West", "Hillcrest/Campus A.M.",
@@ -104,13 +191,21 @@ public class MapFragment extends android.support.v4.app.Fragment implements OnFr
                 "Regents", "SIO Loop", "Sanford Consortium Shuttle", "Coaster West",
                 "East Campus Connector"
         };
-        return buttonName;
+
+        for (int index = 0; index < buttonName.length; index++){
+
+            toReturn.add(index, buttonName[index]);
+
+        }
+
+        return toReturn;
     }
 
 
-    private Integer[] createMapList() {
+    private ArrayList<Integer> createMapList() {
         // ids for the maps that pass through ucsd
         Integer[] mapId;
+        ArrayList<Integer> toReturn = new ArrayList<Integer>();
         mapId = new Integer[]{
                 R.drawable.map201,
                 R.drawable.map202,
@@ -118,34 +213,40 @@ public class MapFragment extends android.support.v4.app.Fragment implements OnFr
                 R.drawable.map30,
                 R.drawable.map41,
                 R.drawable.map237,
-                R.drawable.map921
-
-//                R.drawable.mapcityshuttle,
-//                R.drawable.mapcoastereast,
-//                R.drawable.mapcoastereastwest,
-//                R.drawable.maphillcrestam,
-//                R.drawable.maphillcrestpm,
-//                R.drawable.mapclockwisecampus,
-//                R.drawable.countercampusloop,
-//                R.drawable.eveningclockwise,
-//                R.drawable.eveningcounterclockwise,
-//                R.drawable.mapmesa,
-//                R.drawable.mapregents,
-//                R.drawable.mapsio,
-//                R.drawable.mapsanford,
-//                R.drawable.coasterwest,
-//                R.drawable.eastcampus
+                R.drawable.map921,
+                R.drawable.mapcityshuttle,
+                R.drawable.mapcoastereast,
+                R.drawable.mapcoastereastwest,
+                R.drawable.maphillcrestam,
+                R.drawable.maphillcrestpm,
+                R.drawable.mapclockwisecampus,
+                R.drawable.countercampusloop,
+                R.drawable.eveningclockwise,
+                R.drawable.eveningcounterclockwise,
+                R.drawable.mapmesa,
+                R.drawable.mapregents,
+                R.drawable.mapsio,
+                R.drawable.mapsanford,
+                R.drawable.coasterwest,
+                R.drawable.eastcampus,
 
         };
-        return mapId;
+
+        for (int index = 0; index < mapId.length; index++){
+
+            toReturn.add(index, mapId[index]);
+
+        }
+        return toReturn;
     }
 
 
 
 
-    private Integer[] createIconList(){
+    private ArrayList<Integer> createIconList(){
         // ids for the ucsd or mts icons
         Integer[] iconId;
+        ArrayList<Integer> toReturn = new ArrayList<Integer>();
         iconId = new Integer[]{
                 R.drawable.mtsicon,
                 R.drawable.mtsicon,
@@ -171,7 +272,12 @@ public class MapFragment extends android.support.v4.app.Fragment implements OnFr
                 R.drawable.ucsdicon
 
         };
-        return iconId;
+        for (int index = 0; index < iconId.length; index++){
+
+            toReturn.add(index, iconId[index]);
+
+        }
+        return toReturn;
     }
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
