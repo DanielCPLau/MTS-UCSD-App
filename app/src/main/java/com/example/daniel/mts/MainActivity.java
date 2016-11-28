d255a55ee1c5459dc8bd11009c803e156df15fa3
Isaac Kit Hong Ng <isaacngkh@gmail.com>
2016-11-28 10:56:27 -0800

    .

--- app/src/main/java/com/example/daniel/mts/MainActivity.java
+++ app/src/main/java/com/example/daniel/mts/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // get permission to access network
+//        // get permission to access network
 //        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 //        StrictMode.setThreadPolicy(policy);
 //
