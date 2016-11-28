41b1dfdf95e61398f43dce87fd2777754eaf3ac3
wjddn1994 <jwc048@ucsd.edu>
2016-11-27 17:16:50 -0800

    Update CustomListAdapter.java

--- app/src/main/java/com/example/daniel/mts/CustomListAdapter.java
+++ app/src/main/java/com/example/daniel/mts/CustomListAdapter.java
@@ -20,13 +20,8 @@ import java.util.ArrayList;
 public class CustomListAdapter extends ArrayAdapter<String> {
 
     private final Activity context;
-/*
-    private final String[] linename;
-    private final Integer[] mapid;
-    private final Integer[] iconid;
-    hello
-*/
 
+    //these are arraylist of UI elements
     private final ArrayList<Integer> filterNameArrayColor;
     private final ArrayList<String> filterNameArray;
     private final ArrayList<Integer> filterMapArray;
@@ -37,62 +32,40 @@ public class CustomListAdapter extends ArrayAdapter<String> {
                              ArrayList<Integer> filterIconArray, ArrayList<Integer> filterNameArrayColor) {
 
         super(context, R.layout.map_activity_list, filterNameArray);
-        // TODO Auto-generated constructor stub
-
-
+        //constructor
         this.context=context;
         this.filterNameArray=filterNameArray;
         this.filterMapArray=filterMapArray;
         this.filterIconArray=filterIconArray;
         this.filterNameArrayColor=filterNameArrayColor;
-        /*
-        this.linename=linename;
-        this.mapid=mapid;
-        this.iconid=iconid;
-        */
+       
     }
 
     @Override
     public View getView(int position,View view,ViewGroup parent) {
+        //inflate the view
         LayoutInflater inflater=context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.map_fragment, null,true);
 
-
-
+        //initialize buttons and imageviews into the adapter
         Button lineButton = (Button) rowView.findViewById(R.id.map_button_template);
-
-/*
-        if (lineButton.getText().toString().equals("201") || lineButton.getText().toString().equals(202)){
-            lineButton.setBackgroundColor(Color.RED);
-        }
-        else{
-            lineButton.setBackgroundColor(Color.BLUE);
-        }
-
-*/
-        /*
-        // ucsd busline
-        if (filterMapArray.get(position) == R.drawable.ucsdicon) {
-            lineButton.setBackgroundColor(Color.BLUE);
-        } else { //mts busline
-            lineButton.setBackgroundColor(Color.RED);
-        }*/
-
         ImageView imageView = (ImageView) rowView.findViewById(R.id.map_image);
         ImageView iconImg = (ImageView) rowView.findViewById(R.id.icon_image);
 
-
+        //loading image efficiently
         Picasso.with(context).load(filterMapArray.get(position)).into(imageView);
         Picasso.with(context).load(filterIconArray.get(position)).into(iconImg);
+        
+        //setting the elements' value
         lineButton.setText(filterNameArray.get(position));
         lineButton.setBackgroundColor(filterNameArrayColor.get(position));
-        //imageView.setImageResource(mapid[position]);
-        //iconImg.setImageResource(iconid[position]);
-
+        
+        //when line buttons are pressed
         lineButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                     Button viewButton = (Button) view;
+                    //show MTS bus times
                     if (viewButton.getText().toString().contains("921") ||
                             viewButton.getText().toString().contains("237") ||
                         viewButton.getText().toString().contains("41") ||
@@ -107,6 +80,7 @@ public class CustomListAdapter extends ArrayAdapter<String> {
                         intent.putExtras(dataBundle);
                         context.startActivity(intent);
                     }
+                    // show UCSD bus times
                     else{
                         Uri uriUrl = Uri.parse("https://www.ucsdbus.com/arrivals");
                         Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
